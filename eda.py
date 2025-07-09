34f0a06d778367f36419754cef4c5acc764138f2
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-07-09 15:54:32 +0530

    worklfow

--- eda.py
+++ eda.py
@@ -12,5 +12,14 @@ print(df.head())
 
 print(df.describe())
 
+print(df.isnull().sum())
 
+df_numerical=df.select_dtypes(include=['number'])
+corr=df_numerical.corr()
 
+plt.figure(figsize=(24,30))
+sns.heatmap(corr,annot=True)
+plt.show()
+## so from the heatmap we can observe that Age is highly resulted to the column exited(the output coloumn)
+
+## so we will check whether the data has balanced entries or not
